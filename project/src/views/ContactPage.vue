47a1e25e8ece318734172bdd57da809c67aa1e07
Amelia Leow <58797760+ameliayamato@users.noreply.github.com>
2020-10-27 16:49:36 +0800

    Added LoginPage

---
+++ project/src/views/ContactPage.vue
@@ -0,0 +1,19 @@
+
+<div class="form-group">
+    <label for="email">Your Email Address</label>
+    <input type="text"
+           id="email"
+           class="form-control"
+           v-model="userData.email">
+</div>
+<script>
+    export default {
+        data() {
+            return {
+                userData: {
+                    email: ''
+                }
+            }
+        }
+    }
+</script>
