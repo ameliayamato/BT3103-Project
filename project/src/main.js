5aaab485c79fa48d432adfd76fbf6740d6dc9114
cherylsee16 <cherylsee16@gmail.com>
2020-11-12 04:25:30 +0800

    dist folder

--- project/src/main.js
+++ project/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-import Routes from './route.js'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -118,12 +117,13 @@ const routes = [{
 const router = new VueRouter({
   mode: 'history',
   routes
+})
 
 
 new Vue({
   render: h => h(App), // mount the base component
   store,
-  router: myRouter
+  router
 }).$mount('#app')
 
 
